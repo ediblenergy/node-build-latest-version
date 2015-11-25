427631463bde38748b3ce06361c31ad6d751e816
Samuel Kaufman <samuel.c.kaufman@gmail.com>
2015-11-25 16:02:05 +0000

    change perl deps to be CORE only

--- Makefile
+++ Makefile
@@ -1,18 +1,19 @@
 NODE_VERSION=$(shell ./latest_node_version.pl )
-NODE_VERSION_NAME=node-$(NODE_VERSION)
-NODE_URL=http://nodejs.org/dist/$(NODE_VERSION)/
+NODE_VERSION_NAME=node-v$(NODE_VERSION)
+NODE_PREFIX=node-$(NODE_VERSION)
+NODE_URL=http://nodejs.org/dist/v$(NODE_VERSION)/
 NODE_VERSION_URL=$(NODE_URL)$(NODE_VERSION_NAME).tar.gz
 
 .PHONY: install_latest clean
 
 install_latest: $(NODE_VERSION_NAME)/
-	cd $(NODE_VERSION_NAME) && ./configure --prefix=/opt/$(NODE_VERSION_NAME)
+	cd $(NODE_VERSION_NAME) && ./configure --prefix=/opt/$(NODE_PREFIX)
 	$(MAKE) -C $(NODE_VERSION_NAME)
 	sudo $(MAKE) -C $(NODE_VERSION_NAME) install
 
 $(NODE_VERSION_NAME)/: $(NODE_VERSION_NAME).tar.gz
 	tar -axf $(NODE_VERSION_NAME).tar.gz
-	
+
 $(NODE_VERSION_NAME).tar.gz:
 	wget $(NODE_VERSION_URL)
 
--- latest_node_version.pl
+++ latest_node_version.pl
@@ -1,9 +1,10 @@
 #!/usr/bin/env perl
 use strict;
 use warnings FATAL => 'all';
+
 use FindBin;
-use IPC::System::Simple qw[ capture ];
-use IO::All;
+use HTTP::Tiny;
+use Path::Tiny;
 
 sub get_latest_url {
     my $str = shift;
@@ -15,20 +16,21 @@ sub get_latest_url {
     }
     die "line not found in $str" unless $line;
     my @m = $line =~ /node-v(.*?).tar.gz/;
-    my $version = "v$m[0]";
+    my $version = "$m[0]";
     return $version;
-
 }
+
 sub main {
     my $latest_url = 'http://nodejs.org/dist/latest/';
-    my $cache_file = io->file("$FindBin::Bin/.latest_node_version");
+    my $ua = HTTP::Tiny->new;
+    my $cache_file = path("$FindBin::Bin/.latest_node_version");
     if( -e "$cache_file" ) {
         print "".$cache_file->slurp,"\n";
         return 0;
     }
-    my $res = capture( 'curl', $latest_url );
-    $latest_url = get_latest_url($res);
-    $cache_file->print($latest_url);
+    my $res = $ua->get( $latest_url );
+    $latest_url = get_latest_url($res->{content});
+    $cache_file->spew($latest_url);
     print "$latest_url\n";
     return 0;
 }
